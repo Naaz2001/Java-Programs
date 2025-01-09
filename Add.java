2fc7086214658ee55eafa2524133417c7737286c
Naaz2001 <makandarsheeba@gmail.com>
2025-01-09 20:44:16 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,11 @@
+
+class Add{
+public static void main(String[] args){
+int x = 5;
+int y = 6;
+int z= x + y;
+System.out.println(z);
+System.out.println("Name: SHEEBANAAZ MAKANDAR");
+System.out.println("SAP ID: 500117989");
+    
+}}
